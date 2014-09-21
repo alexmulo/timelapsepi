3e77d48d7f7cf5151b119633fd07bf715723cfcd
alexmulo <alexmulo@gmail.com>
2014-09-21 16:01:03 +0200

    Update lapse.py

--- lapse.py
+++ lapse.py
@@ -7,10 +7,10 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1920,1080)
     camera.hflip=True
     camera.vflip=True
-    time.sleep(300)
     camera.iso=100
     try:
         for i in enumerate(camera.capture_continuous('image{counter:02d}.jpg')):
+            time.sleep(300)
             if i == 576:
                 break
             if  int(datetime.now().strftime("%H"))>=19:
